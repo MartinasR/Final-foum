ed1fd6cee411211d0fe010ecae9dfa8044ecc5ca
MartinasR <martinasrudis@gmail.com>
2022-07-13 09:48:56 +0300

    app.js update

--- Back end/app.js
+++ Back end/app.js
@@ -3,3 +3,29 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require('cors')
+const bodyParser = require("express");
+const whitelist = ['http://localhost:3000', 'http://example2.com'];
+const corsOptions = {
+    credentials: true,
+    origin: (origin, callback) => {
+        if (whitelist.includes(origin))
+            return callback(null, true)
+
+        callback(new Error('Not allowed by CORS'));
+    }
+}
+app.use(cors(corsOptions))
+app.listen(4000);
+app.use(bodyParser.json({limit: "50mb"}));
+app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
+mongoose
+    .connect(process.env.MONGO_KEY)
+    .then((res) => {
+        console.log("connection good");
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+const router = require("./routes/main");
+
+app.use("/", router);
